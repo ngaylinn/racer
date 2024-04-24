Generate graphs, update dist calculation, fix debug visualization, etc.

coevolve.py
@@ -1,8 +1,10 @@
-from abc import ABC, abstractmethod
-
+from neatchi import Neat, NeatControllers, NeatRenderers
 import numpy as np
-import pandas as pd
+import taichi as ti
 from tqdm import trange
+
+import agent
+import constants as c
 
 def select(fitness_scores, count=None):
     if count is None:
@@ -33,54 +35,121 @@ def pair_select(fitness_scores, count=None):
                         select(fitness_scores, count))
     ])
 
-class Domain(ABC):
-    def __init__(self):
-        self.metrics = []
+def fixed_world_assignments():
+    return np.tile(np.arange(c.NUM_INDIVIDUALS), c.NUM_MATCH_UPS)
 
-    @abstractmethod
-    def evaluate(self, interactions):
-        ...
+def random_world_assignments():
+    return np.concatenate([
+        np.random.permutation(c.NUM_INDIVIDUALS)
+        for _ in range(c.NUM_MATCH_UPS)])
 
-    # List of Metrics p_i = f(X_0 ... X_n) -> R
-    # 0 .. n : N roles in this domain.
-    # X_i is a set of entities playing role i (a population)
-    # A tuple (x_0 ... x_n) in X_0 x ... x X_n is an interaction
-    # R is the set of outcomes
+def reduce_fitness(fitness_scores, world_assignments):
+    scores = np.zeros(c.NUM_INDIVIDUALS)
+    for score, individual in zip(fitness_scores, world_assignments):
+        scores[individual] += score
+    return scores / c.NUM_MATCH_UPS
 
-class CoOptimizer(ABC):
-    @abstractmethod
-    def get_interactions(self, scores=None):
-        ...
 
-    @abstractmethod
-    def score_interactions(self, interactions, metrics):
-        ...
+@ti.kernel
+def render_fixed_topology(topo: ti.template()):
+    center = ti.math.vec2(256.0, 256.0)
+    max_dist = ti.math.distance(ti.math.vec2(0.0, 0.0), center)
+    for w, x, y in topo:
+        dist = ti.math.distance((x, y), center) / max_dist
+        topo[w, x, y] = 1.0 - dist**2
 
-    @abstractmethod
-    def overall_score(self, metrics):
-        ...
+@ti.data_oriented
+class PopulationManager:
+    roles = ['topography', 'controller']
+    def __init__(self, expt):
+        self.expt = expt
 
-    @abstractmethod
-    def best_interaction(self, interactions, scores):
-        ...
+        # Neat algorithms for evolving CPPNs for both roles
+        self.neat = {
+            'topography': Neat(
+                num_inputs=2, num_outputs=1,
+                num_individuals=c.NUM_INDIVIDUALS),
+            'controller': Neat(
+                num_inputs=agent.NUM_INPUTS, num_outputs=agent.NUM_OUTPUTS,
+                num_individuals=c.NUM_INDIVIDUALS)
+        }
 
-    # For a Domain...
-    # Generate candidate solutions (an interaction of entities from some subset of roles)
-    # Provide an ordering of candidates
+        # Assignments of CPPNs to simulated worlds.
+        self.world_assignments = {}
 
-def coevolve(domain, cooptimizer, num_iterations):
-    interactions = cooptimizer.get_interactions()
+        # Actuators for interfacing between the CPPNs and the simulation.
+        self.actuators = {
+            'topography': NeatRenderers(
+                num_worlds=c.NUM_WORLDS, num_rows=c.WORLD_SIZE),
+            'controller': NeatControllers(
+                num_worlds=c.NUM_WORLDS, num_activations=c.NUM_OBJECTS)
+        }
+
+    def populate_simulator(self, simulator):
+        self.actuators['topography'].render_all(simulator.topographies)
+        # render_fixed_topology(simulator.topographies)
+        simulator.controllers = self.actuators['controller']
+        simulator.randomize_objects()
+
+    def get_best_individuals(self, scores):
+        world_index = np.argmax(scores['overall'])
+        return (
+            self.actuators['topography'].render_one(
+                self.world_assignments['topography'][world_index],
+                c.WORLD_SHAPE),
+            self.actuators['controller'].get_one(
+                self.world_assignments['controller'][world_index]))
+
+    def update_actuators(self, role):
+        self.world_assignments[role] = random_world_assignments()
+        self.actuators[role].update(
+            self.neat[role].curr_pop, self.world_assignments[role])
+
+    def randomize(self):
+        for role in self.roles:
+            self.neat[role].random_population()
+            self.update_actuators(role)
+
+    def propagate(self, scores):
+        for role in self.roles:
+            selections = pair_select(scores[role])
+            self.neat[role].propagate(selections)
+            self.update_actuators(role)
+
+    def get_scores(self, metrics):
+        return get_scores(metrics, self.expt.fitness, self.world_assignments)
+
+roles = PopulationManager.roles
+
+def get_scores(metrics, fitness, world_assignments=None):
+    if world_assignments is None:
+        world_assignments = {role: [0] for role in roles}
+    assert len(world_assignments) == len(metrics)
+
+    scores = {
+        role: reduce_fitness(
+            fitness[role](metrics),
+            world_assignments[role])
+        for role in roles
+    }
+    scores['overall'] = fitness['overall'](metrics)
+    return scores
+
+def coevolve(simulator, population_manager):
+    population_manager.randomize()
     history = []
 
-    progress = trange(num_iterations)
-    overall_score = 0.0
-    for i in progress:
-        progress.set_description(f'Score == {overall_score:4.2f}')
-        metrics = domain.evaluate(interactions)
-        scores = cooptimizer.score_interactions(metrics)
-        overall_score = cooptimizer.overall_score(metrics)
-        history.append(metrics | scores)
-        if i + 1 < num_iterations:
-            interactions = cooptimizer.get_interactions(scores)
-    return (cooptimizer.best_interaction(interactions, scores),
-            pd.DataFrame(history))
+    progress = trange(c.NUM_GENERATIONS)
+    scores = {'overall': np.zeros(1)}
+    for generation in progress:
+        progress.set_description(f'Score == {scores["overall"].mean():4.2f}')
+
+        population_manager.populate_simulator(simulator)
+        metrics = simulator.run()
+        scores = population_manager.get_scores(metrics)
+
+        history.append({'generation': generation} | metrics | scores)
+        if generation + 1 < c.NUM_GENERATIONS:
+            population_manager.propagate(scores)
+
+    return (population_manager.get_best_individuals(scores), history)

main.py
@@ -1,37 +1,21 @@
-import einops
-from neatchi import Neat, NeatControllers, NeatRenderers
-import numpy as np
-import taichi as ti
-from tqdm import trange
+import functools
+from pathlib import Path
 
-import agent
-from coevolve import Domain, CoOptimizer, coevolve, pair_select
+import einops
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import taichi as ti
+import seaborn as sns
+
+from coevolve import PopulationManager, coevolve, get_scores
 import constants as c
-import fitness
 from simulator import Simulator
 import visualize
 
 # TODO: Increase NUM_TRIALS and restore
 ti.init(arch=ti.cpu, default_fp=ti.f32, default_ip=ti.i32,
         debug=False)
-
-
-def visualize_population(topo_generators, fitness_scores):
-    temp = ti.field(float, shape=(50, 128, 128))
-    topo_generators.render_all(temp)
-    topos = einops.rearrange(
-        temp.to_numpy(), '(gr gc) tr tc -> (gr tr) (gc tc)',
-        gr=10, gc=5)
-    gui = ti.GUI('Racer', (128*10, 128*5),
-                 background_color=0xffffff, show_gui=True)
-    while gui.running:
-        gui.set_image(topos)
-        for i in range(50):
-            x, y = divmod(i, 5)
-            gui.text(f'{fitness_scores[i]:0.3f}',
-                     (x / 10.0, (y + 1) / 5.0), color=0xFF00FF)
-        gui.show()
-
 
 @ti.kernel
 def render_fixed_topology(topo: ti.template()):
@@ -41,241 +25,120 @@ def render_fixed_topology(topo: ti.template()):
         dist = ti.math.distance((x, y), center) / max_dist
         topo[w, x, y] = 1.0 - dist**2
 
+def load_one_experiment_data(experiment):
+    one_experiment_data = pd.DataFrame()
+    for trial in range(c.NUM_TRIALS):
+        trial_data = pd.read_csv(experiment.history_path(trial))
+        trial_data['trial'] = trial
+        one_experiment_data = pd.concat((one_experiment_data, trial_data))
+    return one_experiment_data
 
-@ti.data_oriented
-class RacerDomain(Domain):
-    def __init__(self, simulator):
-        self.simulator = simulator
-        self.dist = ti.field(float, simulator.num_worlds)
-        self.hits = ti.field(float, simulator.num_worlds)
+def visualize_single_experiment(experiment):
+    one_experiment_data = load_one_experiment_data(experiment)
+    fig = sns.relplot(data=one_experiment_data, x='generation', y='score',
+                      hue='role', kind='line')
+    fig.set(title=experiment.name)
+    fig.savefig(f'output/{experiment.name}.png')
+    plt.close()
 
-    @ti.kernel
-    def summarize_simulation(self):
-        self.dist.fill(0.0)
-        self.hits.fill(0.0)
-        for w, o in ti.ndrange(*self.simulator.objects.shape):
-            self.hits[w] += self.simulator.objects[w, o].hits
-            self.dist[w] += self.simulator.objects[w, o].dist
+def load_all_experiments_data():
+    all_experiments_data = pd.DataFrame()
+    for experiment in experiments:
+        one_experiment_data = load_one_experiment_data(experiment)
+        one_experiment_data['experiment'] = experiment.name
+        all_experiments_data = pd.concat((
+            all_experiments_data, one_experiment_data))
+    return all_experiments_data
 
-    def get_metrics(self):
-        self.summarize_simulation()
-        return {
-            'dist': np.nan_to_num(self.dist.to_numpy()),
-            'hits': np.nan_to_num(self.hits.to_numpy())
-        }
+def visualize_all_experiments():
+    all_experiments_data = load_all_experiments_data()
+    all_experiments_data = all_experiments_data.where(
+        all_experiments_data['role'] == 'overall').dropna()
+    fig = sns.relplot(data=all_experiments_data, x='generation', y='score',
+                      hue='experiment', kind='line')
+    fig.set(title='Overall scores across experiments')
+    fig.savefig('output/overall.png')
+    plt.close()
 
-
-    def evaluate(self, interactions):
-        topo_generators, controllers = interactions
-        self.simulator.randomize_objects()
-        #render_fixed_topology(self.simulator.topographies)
-        topo_generators.render_all(self.simulator.topographies)
-        #self.simulator.agents.from_numpy(controllers)
-        self.simulator.controllers = controllers
-        for _ in range(c.NUM_STEPS):
-            self.simulator.step()
-        return self.get_metrics()
-
-class RacerCoOptimizer(CoOptimizer):
-    def __init__(self, name):
+class Experiment:
+    def __init__(self, name, fitness):
         self.name = name
+        self.fitness = {
+            'overall': lambda metrics: metrics['dist'] * metrics['inv_hits']
+        } | fitness
 
-        self.topography_neat = Neat(
-            num_inputs=2, num_outputs=1,
-            num_individuals=c.NUM_INDIVIDUALS)
-        self.topo_generators = NeatRenderers(
-            num_worlds=c.NUM_WORLDS, num_rows=c.WORLD_SIZE)
+    def video_path(self, trial):
+        return Path(f'output/{self.name}_{trial}.mp4')
 
-        self.controller_neat = Neat(
-            num_inputs=agent.NUM_INPUTS, num_outputs=agent.NUM_OUTPUTS,
-            num_individuals=c.NUM_INDIVIDUALS)
-        self.controllers = NeatControllers(
-            num_worlds=c.NUM_WORLDS, num_activations=c.NUM_OBJECTS)
+    def history_path(self, trial):
+        return Path(f'output/{self.name}_{trial}.csv')
 
-    def overall_score(self, metrics):
-        return np.mean(metrics['dist'] / (1 + metrics['hits']))
+    def run(self):
+        print(f'Running {c.NUM_TRIALS} trials of {self.name} '
+              f'with {c.NUM_WORLDS} parallel simulations:')
+        remaining_trials = [
+            trial for trial in range(c.NUM_TRIALS)
+            if not self.history_path(trial).exists()]
+        if len(remaining_trials) == 0:
+            print(f'Already ran {self.name}, reusing saved results.')
+            return
+        for trial in remaining_trials:
+            (best_topography, best_controller), history = coevolve(
+                Simulator(c.NUM_WORLDS), PopulationManager(self))
+            self.record_history(history, trial)
+            self.record_simulation(best_topography, best_controller, trial)
+        visualize_single_experiment(self)
 
+    def record_history(self, history, trial):
+        filtered_history = []
+        for event in history:
+            for role in PopulationManager.roles + ['overall']:
+                # Reorganize the history log so that we keep only the
+                # population-average score for each generation, and break down
+                # scores by role for easy visualization with Seaborn.
+                filtered_history.append({
+                    'generation': event['generation'],
+                    'score': event[role].mean(),
+                    'role': role
+                })
+        df = pd.DataFrame(filtered_history)
+        df.to_csv(self.history_path(trial), index=False)
 
-def fixed_world_assignments():
-    return np.tile(np.arange(c.NUM_INDIVIDUALS), c.NUM_TRIALS)
+    def record_simulation(self, topography, controller, trial):
+        simulator = Simulator()
+        simulator.topographies.from_numpy(
+            einops.repeat(topography, 'w h -> 1 w h'))
+        # render_fixed_topology(simulator.topographies)
+        simulator.controllers = controller
+        # visualize.show(
+        #     simulator,
+        #     functools.partial(get_scores, fitness=self.fitness),
+        #     debug=True)
+        visualize.save(
+            simulator,
+            functools.partial(get_scores, fitness=self.fitness),
+            self.video_path(trial))
 
-def random_world_assignments():
-    return np.concatenate([
-        np.random.permutation(c.NUM_INDIVIDUALS)
-        for _ in range(c.NUM_TRIALS)])
-
-def reduce_fitness(fitness_scores, world_assignments):
-    scores = np.zeros(c.NUM_INDIVIDUALS)
-    for score, individual in zip(fitness_scores, world_assignments):
-        scores[individual] += score
-    return scores / c.NUM_TRIALS
-
-
-class ConditionACoOptimizer(RacerCoOptimizer):
-    def __init__(self):
-        super().__init__('condition_a')
-
-    def get_interactions(self, scores=None):
-        self.topography_world_assignments = random_world_assignments()
-        self.controller_world_assignments = random_world_assignments()
-        if scores is None:
-            self.topo_generators.update(
-                self.topography_neat.random_population(),
-                self.topography_world_assignments)
-            self.controllers.update(
-                self.controller_neat.random_population(),
-                self.controller_world_assignments)
-        else:
-            self.topo_generators.update(
-                self.topography_neat.propagate(
-                    pair_select(scores['topography'])),
-                self.topography_world_assignments)
-            self.controllers.update(
-                self.controller_neat.propagate(
-                    pair_select(scores['controller'])),
-                self.controller_world_assignments)
-        return (self.topo_generators, self.controllers)
-
-    def score_interactions(self, metrics):
-        combined = metrics['dist'] / (1 + metrics['hits'])
-        # self.topography_neat.curr_pop.print_all()
-        # visualize_population(self.topo_generators, combined)
-        return {
-            'topography': reduce_fitness(
-                combined, self.topography_world_assignments),
-            'controller': reduce_fitness(
-                combined, self.controller_world_assignments),
-            'combined': combined
-        }
-
-    def best_interaction(self, interactions, scores):
-        topo_generators, controllers = interactions
-        best_index = np.argmax(scores['combined'])
-        return (topo_generators.render_one(best_index, c.WORLD_SHAPE),
-                controllers.get_one(best_index))
-
-
-class ConditionBCoOptimizer(RacerCoOptimizer):
-    def __init__(self, case):
-        assert case in (1, 2)
-        self.case = case
-        super().__init__(f'condition_b{case}')
-
-    def get_interactions(self, scores=None):
-        self.topography_world_assignments = random_world_assignments()
-        self.controller_world_assignments = random_world_assignments()
-        if scores is None:
-            self.topo_generators.update(
-                self.topography_neat.random_population(),
-                self.topography_world_assignments)
-            self.controllers.update(
-                self.controller_neat.random_population(),
-                self.controller_world_assignments)
-        else:
-            self.topo_generators.update(
-                self.topography_neat.propagate(
-                    pair_select(scores['topography'])),
-                self.topography_world_assignments)
-            self.controllers.update(
-                self.controller_neat.propagate(
-                    pair_select(scores['controller'])),
-                self.controller_world_assignments)
-        return (self.topo_generators, self.controllers)
-
-    def score_interactions(self, metrics):
-        dist = metrics['dist']
-        inv_hits = 1 / (1 + metrics['hits'])
-        return {
-            'topography': reduce_fitness(
-                inv_hits if self.case == 1 else dist,
-                self.topography_world_assignments),
-            'controller': reduce_fitness(
-                dist if self.case == 1 else inv_hits,
-                self.controller_world_assignments),
-            # Note, this hasn't been reduced! It's sized to the number of
-            # worlds, not the number of individuals, so that best_interaction
-            # can find the pairing with the highest combined score.
-            'combined': dist * inv_hits
-        }
-
-    def best_interaction(self, interactions, scores):
-        topo_generators, controllers = interactions
-        world_index = np.argmax(scores['combined'])
-        topography_index = self.topography_world_assignments[world_index]
-        controller_index = self.controller_world_assignments[world_index]
-        return (topo_generators.render_one(topography_index, c.WORLD_SHAPE),
-                controllers.get_one(controller_index))
-
-
-def record_simulation(topography, controller, optimizer, trial):
-    def get_scores(simulator):
-        metrics = RacerDomain(simulator).get_metrics()
-        return optimizer.score_interactions(metrics)
-
-    simulator = Simulator()
-    simulator.topographies.from_numpy(
-        einops.repeat(topography, 'w h -> 1 w h'))
-    #render_fixed_topology(simulator.topographies)
-    simulator.controllers = controller
-    # visualize.show(simulator, get_scores, debug=False)
-    visualize.save(
-        simulator, get_scores, f'output/{optimizer.name}_{trial}.mp4')
-
-
-def summarize_results(history, optimizer, trial):
-    # TODO: Generate charts and stuff.
-    pass
-
-
-def run_experiment(domain, cooptimizer):
-    print(f'Running experiment {cooptimizer.name} '
-          f'with {c.NUM_WORLDS} parallel simulations:')
-    for trial in range(5):
-        (best_topography, best_controller), history = coevolve(
-            domain, cooptimizer, c.NUM_GENERATIONS)
-        record_simulation(
-            best_topography, best_controller, cooptimizer, trial)
-        summarize_results(history, cooptimizer, trial)
-    print('Done!\n')
-
+experiments = [
+    Experiment(
+        name='condition_a',
+        fitness={
+            'topography': lambda metrics: metrics['dist'] * metrics['inv_hits'],
+            'controller': lambda metrics: metrics['dist'] * metrics['inv_hits']}),
+    Experiment(
+        name='condition_b1',
+        fitness={
+            'topography': lambda metrics: metrics['inv_hits'],
+            'controller': lambda metrics: metrics['dist']}),
+    Experiment(
+        name='condition_b2',
+        fitness={
+            'topography': lambda metrics: metrics['dist'],
+            'controller': lambda metrics: metrics['inv_hits']}),
+]
 
 if __name__ == '__main__':
-    simulator = Simulator(c.NUM_WORLDS)
-    domain = RacerDomain(simulator)
-    run_experiment(domain, ConditionACoOptimizer())
-    run_experiment(domain, ConditionBCoOptimizer(1))
-    run_experiment(domain, ConditionBCoOptimizer(2))
-
-    exit()
-
-    np.random.seed(42)
-    evaluator = fitness.Speedy(c.NUM_WORLDS)
-    # try:
-    #     best_agent = np.load('controller.npy')
-    #     best_topo = np.load('topography.npy')
-    # except Exception:
-    best_agent, best_topo = evolve(evaluator)
-    np.save('controller.npy', best_agent)
-    np.save('topography.npy', best_topo)
-
-    # Random agent
-    # best_agent = agent.randomize(1)[0]
-
-    # Null agent (does nothing)
-    best_agent = np.zeros((), dtype=agent.AGENT_DTYPE)
-
-    # Fixed topography
-    # render_fixed_topology(simulator.topo)
-
-    # Null topography (empty space)
-    # np.zeros(c.WORLD_SHAPE, dtype=np.float32)
-
-    simulator = Simulator()
-    simulator.topographies.from_numpy(
-        einops.repeat(best_topo, 'w h -> 1 w h'))
-    simulator.agents.from_numpy(
-        einops.repeat(np.array([best_agent]), '1 -> 1 o', o=c.NUM_OBJECTS))
-
-    visualize.show(simulator, debug=False)
-    # visualize.save(simulator, 'output/sample.mp4')
-    # visualize.save_batch(simulator, 'output/sample{i}.mp4', 5)
+    sns.set_style('darkgrid')
+    for experiment in experiments:
+        experiment.run()
+    visualize_all_experiments()

simulator.py
@@ -19,6 +19,8 @@ class Simulator:
         self.avg_pos = ti.math.vec2.field(shape=(num_worlds))
         self.avg_vel = ti.math.vec2.field(shape=(num_worlds))
         self.views = agent.View.field(shape=(num_worlds, c.NUM_OBJECTS))
+        self.reactions = agent.Reaction.field(
+            shape=(num_worlds, c.NUM_OBJECTS))
 
     def randomize_objects(self):
         shape = (self.num_worlds, c.NUM_OBJECTS)
@@ -32,6 +34,9 @@ class Simulator:
         self.objects.rad.fill(0.0)
         self.objects.hits.fill(0)
         self.objects.dist.fill(0.0)
+        # To calculate displacement, we peek at the velocity values from the
+        # objects in the previous step, so initialize to 0.0.
+        self.__objects.vel.fill(0.0)
 
     @ti.func
     def project_vec(from_vec, onto_vec):
@@ -75,59 +80,64 @@ class Simulator:
             self.avg_vel[w] += obj.vel / c.NUM_OBJECTS
 
     @ti.kernel
-    def __update_views(self, objects: ti.template()):
+    def __update_views(self, objects: ti.template(), debug: bool):
         for w, o1 in ti.ndrange(self.num_worlds, c.NUM_OBJECTS):
+            object1 = objects[w, o1]
+            view = agent.View(0.0)
             # Record proprioception data (per object view inputs).
-            self.views[w, o1][agent.POS_X] = objects[w, o1].pos.x
-            self.views[w, o1][agent.POS_Y] = objects[w, o1].pos.y
-            self.views[w, o1][agent.VEL_X] = objects[w, o1].vel.x
-            self.views[w, o1][agent.VEL_Y] = objects[w, o1].vel.y
+            view[agent.POS_X] = object1.pos.x
+            view[agent.POS_Y] = object1.pos.y
+            view[agent.VEL_X] = object1.vel.x
+            view[agent.VEL_Y] = object1.vel.y
 
             # Record the force acting on the object based on the incline of the
             # topography at its current position.
-            acc = self.acc_from_topography(w, objects[w, o1].pos)
-            self.views[w, o1][agent.SLP_X] = acc.x
-            self.views[w, o1][agent.SLP_Y] = acc.y
+            acc = self.acc_from_topography(w, object1.pos)
+            view[agent.SLP_X] = acc.x
+            view[agent.SLP_Y] = acc.y
 
             # Zero out all the nearest view values, so we don't get historical
             # values hanging around because an object noved out of view.
-            self.views[w, o1][agent.NRST_POS_X] = 0.0
-            self.views[w, o1][agent.NRST_POS_Y] = 0.0
-            self.views[w, o1][agent.NRST_VEL_X] = 0.0
-            self.views[w, o1][agent.NRST_VEL_X] = 0.0
+            view[agent.NRST_POS_X] = 0.0
+            view[agent.NRST_POS_Y] = 0.0
+            view[agent.NRST_VEL_X] = 0.0
+            view[agent.NRST_VEL_X] = 0.0
 
             # Record the relative position and velocity of the nearest object.
             min_distance = ti.math.inf
             # TODO: Optimize?
             for o2 in range(c.NUM_OBJECTS):
                 if o1 != o2:
-                    distance = ti.math.distance(
-                        objects[w, o1].pos, objects[w, o2].pos)
+                    object2 = objects[w, o2]
+                    distance = ti.math.distance(object1.pos, object2.pos)
                     if distance < c.VIEW_RADIUS and distance < min_distance:
-                        rel_pos = objects[w, o2].pos - objects[w, o1].pos
-                        rel_vel = objects[w, o2].vel - objects[w, o1].vel
+                        rel_pos = object2.pos - object1.pos
+                        rel_vel = object2.vel - object1.vel
                         min_distance = distance
-                        self.views[w, o1][agent.NRST_POS_X] = rel_pos.x
-                        self.views[w, o1][agent.NRST_POS_Y] = rel_pos.y
-                        self.views[w, o1][agent.NRST_VEL_X] = rel_vel.x
-                        self.views[w, o1][agent.NRST_VEL_X] = rel_vel.y
+                        view[agent.NRST_POS_X] = rel_pos.x
+                        view[agent.NRST_POS_Y] = rel_pos.y
+                        view[agent.NRST_VEL_X] = rel_vel.x
+                        view[agent.NRST_VEL_X] = rel_vel.y
 
             # Record relative positions to the centroid of all objects.
-            rel_pos = self.avg_pos[w] - objects[w, o1].pos
-            rel_vel = self.avg_vel[w] - objects[w, o1].vel
-            self.views[w, o1][agent.AVG_POS_X] = rel_pos.x
-            self.views[w, o1][agent.AVG_POS_Y] = rel_pos.y
+            rel_pos = self.avg_pos[w] - object1.pos
+            rel_vel = self.avg_vel[w] - object1.vel
+            view[agent.AVG_POS_X] = rel_pos.x
+            view[agent.AVG_POS_Y] = rel_pos.y
 
             # Let this agent react to the view we just computed.
-            #reaction = self.agents[w, o1].react(self.views[w, o1])
-            reaction = self.controllers.activate(self.views[w, o1], w, o1)
-            objects[w, o1].acc.x = acc.x + 1.2 * reaction[agent.ACC_X]
-            objects[w, o1].acc.y = acc.y + 1.2 * reaction[agent.ACC_Y]
-            objects[w, o1].rad = reaction[agent.RAD]
+            reaction = self.controllers.activate(view, w, o1)
+            object1.acc.x = acc.x + 1.2 * reaction[agent.ACC_X]
+            object1.acc.y = acc.y + 1.2 * reaction[agent.ACC_Y]
+            object1.rad = reaction[agent.RAD]
 
-    def view_and_react(self):
+            objects[w, o1] = object1
+            self.views[w, o1] = view
+            self.reactions[w, o1] = reaction
+
+    def view_and_react(self, debug=False):
         self.__update_averages(self.objects)
-        self.__update_views(self.objects)
+        self.__update_views(self.objects, debug)
 
     @ti.func
     def wall_collide(self, objects: ti.template(), w, o):
@@ -153,10 +163,14 @@ class Simulator:
     @ti.func
     def object_collide(self, objects: ti.template(), w, o1):
         # Look at all other objects to see if they collide with this one.
+        # NOTE: This is a very simple collision detection and resolution
+        # algorithm. It's not very realistic, merely good enough for this
+        # experiment.
         # TODO: Optimize?
         for o2 in range(c.NUM_OBJECTS):
-            if o1 == o2:
-                break
+            # Only consider each pair of objects once. 
+            if o1 <= o2:
+                continue
             pos1, pos2 = objects[w, o1].pos, objects[w, o2].pos
             vec = pos1 - pos2
             distance = vec.norm()
@@ -181,7 +195,11 @@ class Simulator:
     def __update_objects(self, prev_objects: ti.template(),
                          next_objects: ti.template()):
         for w, o in ti.ndrange(self.num_worlds, c.NUM_OBJECTS):
-            next_objects[w, o] = prev_objects[w, o].next_state()
+            # Since we haven't updated this object in next_objects, it's still
+            # the object from the step before. Exploit this to get the previous
+            # velocity, which is used to calculate displacement.
+            prev_vel = next_objects[w, o].vel
+            next_objects[w, o] = prev_objects[w, o].next_state(prev_vel)
             self.wall_collide(next_objects, w, o)
             self.object_collide(next_objects, w, o)
 
@@ -189,6 +207,26 @@ class Simulator:
         self.__update_objects(self.objects, self.__objects)
         self.objects, self.__objects = self.__objects, self.objects
 
+    @ti.kernel
+    def get_metrics_kernel(self, dist: ti.template(), hits: ti.template()):
+        for w, o in ti.ndrange(*self.objects.shape):
+            dist[w] += self.objects[w, o].dist
+            hits[w] += self.objects[w, o].hits
+
+    def get_metrics(self):
+        dist = ti.field(float, self.num_worlds)
+        hits = ti.field(float, self.num_worlds)
+        self.get_metrics_kernel(dist, hits)
+        return {
+            'dist': np.nan_to_num(dist.to_numpy()),
+            'inv_hits': 1 / (1 + np.nan_to_num(hits.to_numpy()))
+        }
+
     def step(self):
         self.view_and_react()
         self.update_objects()
+
+    def run(self):
+        for _ in range(c.NUM_STEPS):
+            self.step()
+        return self.get_metrics()