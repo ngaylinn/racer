Fix fitness calculation, trial indices. Refactor log output, score visualization.

coevolve.py
@@ -12,6 +12,8 @@ generating, simulating, and breeding populations is provided by the
 PopulationManager class.
 """
 
+from dataclasses import dataclass
+
 import numpy as np
 import pandas as pd
 from tqdm import trange
@@ -41,14 +43,30 @@ def select(fitness_scores):
     return result
 
 
+def shuffled(array):
+    return np.random.choice(array, size=len(array), replace=False)
+
+
 def pair_select(fitness_scores):
     return np.array([
         # In every breeding pair, the most fit is considered the "parent" and
         # the other is the "mate." This is used by the Neat algorithm to
         # introduce a small bias towards more fit individuals in crossover.
         [p, m] if fitness_scores[p] > fitness_scores[m] else [m, p]
-        for p, m in zip(select(fitness_scores), select(fitness_scores))
+        for p, m in zip(select(fitness_scores),
+                        shuffled(select(fitness_scores)))
     ], dtype=np.int32)
+
+
+@dataclass
+class LogData:
+    # Objective summaries of the simulation
+    metrics: pd.DataFrame
+    # Subjective fitness of individuals simulated
+    scores: pd.DataFrame
+    # Match-making history of individuals simulated
+    genealogy: pd.DataFrame
+    # TODO: Add population diversity metrics?
 
 
 def coevolve(simulator, population_manager, fitness):
@@ -60,6 +78,7 @@ def coevolve(simulator, population_manager, fitness):
     overall_score = 0.0
     all_metrics = []
     all_scores = []
+    all_genealogies = []
 
     # Evolve the random populations
     for generation in progress:
@@ -70,6 +89,11 @@ def coevolve(simulator, population_manager, fitness):
         metrics['generation'] = generation
         all_metrics.append(metrics)
 
+        # TODO: Optimize? This is actually the slowest part of the whole process,
+        # since the next generation can't run until scores are returned to Python,
+        # fitness is calculated, and matches are pushed back to Taichi on the GPU.
+        # That back and forth can only be avoided by computing fitness scores and
+        # doing selection on the GPU.
         scores = population_manager.get_scores(fitness, metrics)
         scores['generation'] = generation
         scores.to_csv('scores.csv', index=False)
@@ -77,10 +101,18 @@ def coevolve(simulator, population_manager, fitness):
         all_scores.append(scores)
 
         if generation + 1 < c.NUM_GENERATIONS:
-            population_manager.propagate(scores)
+            genealogy = population_manager.propagate(scores)
+            genealogy['generation'] = generation
+            all_genealogies.append(genealogy)
 
     best_row = fitness['overall'](metrics).argmax()
-    # Currently, the index column corresponds with the values in the world
-    # column, but I still look up the world just in case that ever changes.
+    # Currently, the index values correspond with the values in the world
+    # column, but look up the world just in case that ever changes.
     best_world_index = int(metrics.loc[best_row]['world'])
-    return (best_world_index, pd.concat(all_metrics), pd.concat(all_scores))
+
+    logs = LogData(
+        pd.concat(all_metrics),
+        pd.concat(all_scores),
+        pd.concat(all_genealogies)
+    )
+    return (best_world_index, logs)

population_manager.py
@@ -41,30 +41,49 @@ def pair_select_per_trial(scores):
     within a single Neat population, enforcing that individuals in different
     trials never mate without exposing that constraint to the Neatchi library.
     """
-    matches_per_trial = [
-        pair_select(df['fitness'].to_numpy()) + (trial * c.NUM_INDIVIDUALS)
-        for trial, df in scores.groupby('trial')
-    ]
-    return np.concatenate(matches_per_trial)
+    frames = []
+    selections_per_trial = []
+    for trial, trial_df in scores.groupby('trial'):
+        selections = pair_select(trial_df['fitness'].to_numpy())
+        frames.append(pd.DataFrame({
+            'individual': np.arange(c.NUM_INDIVIDUALS, dtype=np.int32),
+            'trial': trial,
+            'parent': selections[:, 0],
+            'mate': selections[:, 1],
+        }))
+
+        # Trials are just sub-populations within a single NeatPopulation, so
+        # translate from trial-relative indexing to absolute indexing.
+        np.add(selections, trial * c.NUM_INDIVIDUALS, out=selections)
+        selections_per_trial.append(selections)
+
+    return np.concatenate(selections_per_trial), pd.concat(frames)
 
 
 def fixed_world_assignments():
-    return np.tile(
-        np.arange(c.NUM_INDIVIDUALS, dtype=np.int32),
-        c.NUM_TRIALS * c.NUM_MATCH_UPS)
+    return np.concatenate([
+        # Assign individuals to the same NUM_MATCHUPS pairings for each trial.
+        np.tile(
+            np.arange(c.NUM_INDIVIDUALS, dtype=np.int32),
+            c.NUM_MATCH_UPS
+        # Trials are just sub-populations within a single NeatPopulation, so
+        # translate from trial-relative indexing to absolute indexing.
+        ) + trial * c.NUM_INDIVIDUALS
+        for trial in range(c.NUM_TRIALS)])
 
 
 def random_world_assignments():
     return np.concatenate([
-        np.random.permutation(c.NUM_INDIVIDUALS)
-        for _ in range(c.NUM_TRIALS * c.NUM_MATCH_UPS)], dtype=np.int32)
-
-
-def index_dict_of_arrays(dict_of_arrays, index):
-    return {
-        key: values[index]
-        for key, values in dict_of_arrays.items()
-    }
+        # Randomly assign individuals to NUM_MATCHUPS pairings for each trial.
+        np.concatenate([
+            np.random.permutation(c.NUM_INDIVIDUALS).astype(np.int32) \
+            # Trials are just sub-populations within a single NeatPopulation,
+            # so translate from trial-relative indexing to absolute indexing.
+            + trial * c.NUM_INDIVIDUALS
+            for _ in range(c.NUM_MATCH_UPS)
+        ])
+        for trial in range(c.NUM_TRIALS)
+    ])
 
 
 @ti.data_oriented
@@ -115,11 +134,22 @@ class PopulationManager:
             self.update_actuators(role)
 
     def propagate(self, scores):
+        all_genealogies = []
         for role in self.roles:
-            selections = pair_select_per_trial(
+            # Get selections and genealogy log.
+            selections, genealogy = pair_select_per_trial(
                 scores[scores['role'] == role])
+
+            # Apply selections to the population / actuators.
+            # TODO: Ideally, we should record which matches performed crossover
+            # in the genealogy.
             self.neat[role].propagate(selections)
             self.update_actuators(role)
+
+            # Log the feel genealogy of the populations.
+            genealogy['role'] = role
+            all_genealogies.append(genealogy)
+        return pd.concat(all_genealogies)
 
     def annotate_metrics(self, metrics):
         # Associate each world with the individuals of each role that
@@ -132,22 +162,24 @@ class PopulationManager:
 
     def get_scores(self, fitness, metrics):
         frames = []
-        for trial, trial_df in metrics.groupby('trial'):
-            for role in self.roles:
-                frames.append(pd.DataFrame({
-                    'trial': trial,
-                    'role': role,
-                    'individual': np.arange(c.NUM_INDIVIDUALS),
-                    # This averages metrics across instances THEN computes
-                    # fitness, which is much more efficient than computing
-                    # fitness then averaging the results, but may not work for
-                    # other fitness functions.
-                    'fitness': fitness[role](
-                        trial_df.groupby(f'{role}_index').mean())
-                }))
-            frames.append(pd.DataFrame([{
-                'trial': trial,
-                'role': 'overall',
-                'fitness': fitness['overall'](trial_df).mean()
-            }]))
+        for role in self.roles:
+            # Calculate the role-specific fitness score for all worlds, then
+            # average each individual's score across all match-ups in each of
+            # the trials.
+            frames.append(pd.DataFrame({
+                'trial': metrics['trial'].to_numpy(),
+                'role': role,
+                'individual': metrics[f'{role}_index'].to_numpy(),
+                'fitness': fitness[role](metrics)
+            }).groupby(['trial', 'role', 'individual']).mean().reset_index())
+
+        # Also calculate an overall score for each trial, not attributed to any
+        # individuals.
+        frames.append(pd.DataFrame({
+            'trial': metrics['trial'].to_numpy(),
+            'role': 'overall',
+            'individual': None,
+            'fitness': fitness['overall'](metrics)
+        }).groupby(['trial', 'role']).mean().reset_index())
+
         return pd.concat(frames)

simulator.py
@@ -263,11 +263,11 @@ class Simulator:
         hits = np.zeros(c.NUM_WORLDS, dtype=np.float32)
         self.get_metrics_kernel(ang_disp, lin_disp, hits)
         return pd.DataFrame({
-            'world': w,
-            'ang_disp': ang_disp[w],
-            'lin_disp': lin_disp[w],
-            'hits': hits[w]
-        } for w in range(c.NUM_WORLDS))
+            'world': np.arange(c.NUM_WORLDS),
+            'ang_disp': ang_disp,
+            'lin_disp': lin_disp,
+            'hits': hits
+        })
 
     def step(self):
         self.view_and_react()